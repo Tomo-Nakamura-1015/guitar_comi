fce951f52e4ba80eaf45c4f78f184f3b76838ca6
github user name <tomodoranicof@gmail.com>
2020-06-16 17:20:58 +0900

    css更新

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
-  validates :subject, presence: true, length: {maximum:300}
+  validates :subject, presence: true, length: {maximum:400}
 end
